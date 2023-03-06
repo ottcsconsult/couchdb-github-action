f75783e0b341c37558c484fadf5c0649738fcc8f
Christian D Ott <cott@miniguendo.local>
2023-03-06 00:08:42 -0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,19 +10,16 @@ COUCHDB_VERSION=$1
 
 
 echo "Starting CouchDB..."
-pwd
+#pwd
 cp -r /tmp/couch/* .
-ls -lrta ./
+# ls -lrta ./
 docker build -t appro-couchdb .
 docker run --name appro-couchdb -p 5984:5984 -d appro-couchdb
 docker ps 
 
-sleep 2
-docker ps
-
-docker logs `docker ps | awk '{ print $1 }' | head -n 2|tail -n 1`
-
-sleep 5
-
-docker ps
+sleep 8
+#docker ps
+#docker logs `docker ps | awk '{ print $1 }' | head -n 2|tail -n 1`
+#sleep 5
+#docker ps
 
